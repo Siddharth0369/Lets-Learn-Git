b45bfcb5a7be6f79af02ec09ddc34cc1fb94cb3a
Siddharth Ravi Kumar <siddharthravikumar3@gmail.com>
2022-07-08 13:59:07 +0530

    adding Sum.java

--- .history/sum_20220708134630.java
+++ .history/sum_20220708134630.java
@@ -1,8 +1,6 @@
 public class sum {
     public static void main(String[] args){
         int a = 5;
-        int b = 6;
-
-        System.out.println("The Sum is: " + (a+b));
+        int b = 
     }
 }
